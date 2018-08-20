8e9f311d887d62b0925687630afd376ef04861b1
Chaofan <zchaofan08@gmail.com>
2018-08-20 21:54:07 +1000

    jplayer play success

--- 20180818MusicPlayer/app/root.js
+++ 20180818MusicPlayer/app/root.js
@@ -6,7 +6,7 @@ import Progress from './components/progress'
 let Root = React.createClass({
   getInitialState: function() {
     return {
-      progress: '-' 
+      progress: '-'
     };
   },
   // mp3: 'https://freemusicdownloads.world/wp-content/uploads/2017/05/Justin-Bieber-Sorry-PURPOSE-The-Movement.mp3'
@@ -16,8 +16,8 @@ let Root = React.createClass({
       ready: function() {
         console.log('start');
         $(this).jPlayer('setMedia', {
-          mp3: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
-        }).jPlayer('play', 0);
+          mp3: 'http://www.jplayer.org/audio/mp3/Miaow-snip-Stirring-of-a-fool.mp3'
+        }).jPlayer('play');
       },
       supplied: 'mp3',
       wmode: 'window'
@@ -25,11 +25,14 @@ let Root = React.createClass({
     console.log('bind');
     // bind the play progress to a recall function (refresh the state of the component)
     $('#player').bind($.jPlayer.event.timeupdate, (e) => {
-    	console.log('progress');
+    	// console.log('progress');
       this.setState({
         progress: Math.round(e.jPlayer.status.currentTime)
       });
     });
+    $('#player').bind($.jPlayer.event.play, function(event) {
+      console.log('start play');
+    })
   },
   render() {
     return(

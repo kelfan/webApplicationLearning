9d39e8f7a335d34922f7a73d15e89adc2f0c5199
Chaofan <zchaofan08@gmail.com>
2018-08-21 21:03:58 +1000

    progressbar onclick

--- 20180818MusicPlayer/app/components/progress.js
+++ 20180818MusicPlayer/app/components/progress.js
@@ -2,9 +2,15 @@ import React from 'react'
 import './progress.less'
 
 let Progress = React.createClass({
+  changeProgress(e) {
+    let progressBar = this.refs.progressBar;
+    let progress = (e.clientX - progressBar.getBoundingClientRect().left) / progressBar.clientWidth;
+    console.log(progress);
+    
+  }, 
   render(){
     return (
-      <div className="components-progress">
+      <div className="components-progress" ref="progressBar" onClick={this.changeProgress}>
         <div className="progress" style={{width: `${this.props.progress}%`}} ></div>
       </div>
     );

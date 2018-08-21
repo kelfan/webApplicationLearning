8ccd9352553d5aa2485fb06dd502d6427dc2c4dc
Chaofan <zchaofan08@gmail.com>
2018-08-21 22:50:24 +1000

    change page UI

--- 20180818MusicPlayer/app/page/player.js
+++ 20180818MusicPlayer/app/page/player.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Progress from '../components/progress'
+import './player.less'
+import { Link } from 'react-router';
 
 let duration = null;
 let Player = React.createClass({
     getInitialState: function () {
         return {
-            progress: '-'
+            progress: 0,
+			volume: 0,
+			isPlay: true,
+			leftTime: ''
         };
     },
     componentDidMount() {
@@ -26,13 +31,40 @@ let Player = React.createClass({
     },
     render() {
         return (
-            <div>
-                <Progress
-                    progress={this.state.progress}
-                    onProgressChange={this.progressChangeHandler}
-                    barColor="#ff0000"
-                ></Progress>
+            <div className="player-page">
+            <h1 className="caption"><Link to="/list">My Music Player &gt;</Link></h1>
+            <div className="mt20 row">
+                <div className="controll-wrapper">
+                    <h2 className="music-title">song name</h2>
+                    <h3 className="music-artist mt10">singer</h3>
+                    <div className="row mt20">
+                        <div className="left-time -col-auto">left time</div>
+                        <div className="volume-container">
+                            <i className="icon-volume rt" style={{top: 5, left: -5}}></i>
+                            <div className="volume-wrapper">
+                                volume control part 
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{height: 10, lineHeight: '10px'}}>
+                        play progress part 
+                    </div>
+                    <div className="mt35 row">
+                        <div>
+                            <i className="icon prev"></i>
+                            <i className='icon ml20 play'></i>
+                            <i className="icon next ml20"></i>
+                        </div>
+                        <div className="-col-auto">
+                            <i className='icon repeat-cycle' ></i>
+                        </div>
+                    </div>
+                </div>
+                <div className="-col-auto cover">
+                    <img src='' alt='song name'/>
+                </div>
             </div>
+        </div>
         )
     }
 

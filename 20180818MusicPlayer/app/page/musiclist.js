03d1fe3fec6cb9779f7c47a74a1ac45486898ba1
Chaofan <zchaofan08@gmail.com>
2018-08-22 12:43:49 +1000

    change music list style

--- 20180818MusicPlayer/app/page/musiclist.js
+++ 20180818MusicPlayer/app/page/musiclist.js
@@ -1,17 +1,25 @@
 import React from 'react'
+import MusicListItem from '../components/musiclistitem'
 
 let MusicList = React.createClass({
     render() {
         let listEle = null;
-        listEle = this.props.musicList.map((item)=>{
-            return <li key={item.id}>{item.title}</li>  
+        listEle = this.props.musicList.map((item) => {
+            return (
+                <MusicListItem
+                    key={item.id}
+                    musicItem={item}
+                >
+                    {item.title}
+                </MusicListItem>
+            )
         });
 
         return (
             <ul>
                 {listEle}
             </ul>
-        )
+        );
     }
 });
 

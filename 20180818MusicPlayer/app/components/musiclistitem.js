bc425a06241a32288d70ffa63a55528ad51c2efb
Chaofan <zchaofan08@gmail.com>
2018-08-22 12:50:31 +1000

    hightlight playing song

--- 20180818MusicPlayer/app/components/musiclistitem.js
+++ 20180818MusicPlayer/app/components/musiclistitem.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './musiclistitem.less'
 
 let MusicListItem = React.createClass({
-    render(){
+    render() {
         let musicItem = this.props.musicItem;
         return (
-            <li className="components-listitem row">
+            <li className={`components-listitem row${this.props.focus ? ' focus' : ''}`}>
                 <p><strong>{musicItem.title}</strong> - {musicItem.artist}</p>
                 <p className="-col-auto delete"></p>
             </li>
--- 20180818MusicPlayer/app/page/musiclist.js
+++ 20180818MusicPlayer/app/page/musiclist.js
@@ -7,6 +7,7 @@ let MusicList = React.createClass({
         listEle = this.props.musicList.map((item) => {
             return (
                 <MusicListItem
+                    focus={item===this.props.currentMusicItem}
                     key={item.id}
                     musicItem={item}
                 >
